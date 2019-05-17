725d1eae92c693439891c4c8a340c2ad9513d288
bluelogix <bluelogix010@gmail.com>
2019-05-17 12:15:25 -0400

    ring buffer passing test

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,14 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    self.storage[self.current] = item
+    self.current = (self.current+1) % self.capacity
+   
 
   def get(self):
-    pass
+    x = [i for i in self.storage if i is not None]
+    return x
+   
+
+
+    
